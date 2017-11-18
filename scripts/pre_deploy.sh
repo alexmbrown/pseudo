4ed718f17d1003fc36f01396dd27bec74c139286
Alex Brown <alex.m.brown@ehi.com>
2017-11-18 08:11:45 -0600

    update pre deploy script to correctly zip artifacts

--- scripts/pre_deploy.sh
+++ scripts/pre_deploy.sh
@@ -1,6 +1,7 @@
-for path in packages/*; do
+cd packages
+for path in *; do
     [ -d "${path}" ] || continue # if not a directory, skip
     dirname="$(basename "${path}")"
     echo "${path}"
-    zip -r "${dirname}.zip" $path
+    zip -r "../${dirname}.zip" $path
 done
